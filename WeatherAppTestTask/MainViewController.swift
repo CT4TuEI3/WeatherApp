8db3676a30c8ed70785c253d6192c09e38bd6433
Alexey Statiev <31804144+CT4TuEI3@users.noreply.github.com>
2023-05-24 17:36:34 +0300

    Add files via upload

---
+++ WeatherAppTestTask/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  WeatherAppTestTask
+//
+//  Created by Алексей on 24.05.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
